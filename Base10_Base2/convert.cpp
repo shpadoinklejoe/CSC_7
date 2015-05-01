1655e90bb141e7c3a91de02594156f26002f4915
Joseph Gunderson <shpadoinklejoe@yahoo.com>
2015-04-30 17:55:39 -0700

    base 10 to hexadecimal

--- Base10_Base2/convert.cpp
+++ Base10_Base2/convert.cpp
@@ -3,11 +3,23 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <sstream>
+
+
 
 using namespace std;
 
 // Prints vector backwards
 void print(const vector<int>& v)
+{
+    for(int i=v.size()-1; i>=0; i--)
+    {
+        cout << v[i];
+    }
+}
+
+// Prints vector backwards
+void print(const vector<string>& v)
 {
     for(int i=v.size()-1; i>=0; i--)
     {
@@ -33,6 +45,52 @@ void toBinary (unsigned int n)
     print(v); 
 }
 
+// Convert Base(10) => Base(16)
+void toHexadecimal(int numerator)
+{
+    vector<string>v;
+    
+    if(numerator==0){
+        v.push_back("0");
+    }
+    
+    while ( numerator > 0 )
+    {
+        int num = numerator%16;
+        
+        if (num < 10){
+        string number;
+        ostringstream convert;
+        convert << num;
+        number = convert.str();
+            v.push_back(number);
+        }
+        else if (num == 10){
+            v.push_back("A");
+        }
+        else if (num == 11){
+            v.push_back("B");
+        }
+        else if (num == 12){
+            v.push_back("C");
+        }
+        else if (num == 13){
+            v.push_back("D");
+        }
+        else if (num == 14){
+            v.push_back("E");
+        }
+        else if (num == 15){
+            v.push_back("F");
+        }
+        
+        
+        numerator = numerator/16;
+    }
+    
+    print(v);
+}
+
 /*
  * 
  */
@@ -48,6 +106,11 @@ int main(int argc, char** argv) {
     cout << " in Binary\n";
     
     
+    int base10 = 287;
+    
+    cout << base10 <<" = ";
+    toHexadecimal(base10);
+    cout << " in Hexadecimal\n";
 
     return 0;
 }
